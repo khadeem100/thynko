6f5d0c9d89121afa41023a3367e9c8ec20c512f1
khadeem100 <khadeem.smart@outlook.com>
2025-08-29 14:54:33 +0200

    yetti

---
+++ packages/prisma/client.ts
@@ -0,0 +1,22 @@
+export * from "@prisma/client";
+
+export {
+  CommissionStatus,
+  CommissionType,
+  EventType,
+  FolderType,
+  FolderUserRole,
+  InvoiceStatus,
+  PartnerBannedReason,
+  PartnerLinkStructure,
+  PartnerProfileType,
+  PartnerRole,
+  PartnerStatus,
+  PartnerUrlValidationMode,
+  PayoutStatus,
+  Prisma,
+  ProgramEnrollmentStatus,
+  RewardStructure,
+  Role,
+  WebhookReceiver,
+} from "@prisma/client";
